c16036072394954ddee31bee3d42ac01dc4fd242
Miquel Stam <mql.stam@gmail.com>
2023-04-18 09:47:25 +0200

    Add POST request handler and body parsing

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
 
 app.get('/',(req,res) => {
     res.send({"text":"Hello World!"});
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+
 app.post('/', (req, res) => {
+  console.log(req.body);
   res.send('Got a POST request')
 })
 app.put('/user', (req, res) => {
@@ -17,4 +19,8 @@ app.put('/user', (req, res) => {
 })
 app.delete('/user', (req, res) => {
   res.send('Got a DELETE request at /user')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 })
