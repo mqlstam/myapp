4828ba4f0a9f6dce667372b4eb7fd4c4d3d2e11e
Miquel Stam <mql.stam@gmail.com>
2023-04-24 23:13:28 +0200

    add inmemory database

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3000;
 
-const database = {};
-let nextId = 0;
+const database = {
+  users: {},
+  meals: {},
+};
+let nextUserId = 0;
+let nextMealId = 0;
 const saltRounds = 10;
 
 const jwtSecret = 'your-secret-key';
-
 
 const validateEmail = (email) => {
   return String(email)
@@ -21,31 +24,28 @@ const validateEmail = (email) => {
 };
 
 const emailExists = (email) => {
-  for (let id in database) {
-    if (database[id].email === email) {
-      return true; // e-mail is al in database
+  for (let id in database.users) {
+    if (database.users[id].email === email) {
+      return true;
     }
   }
-  return false; // e-mail is niet in database
+  return false;
 };
-
 
 app.use(bodyParser.json());
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.send(database);
-})
+});
 
 app.get('/user/:id', (req, res) => {
   const { id } = req.params;
-  if (!database[id]) {
-    res.status(404).send({ error: `Email with id ${id} not found` });
+  if (!database.users[id]) {
+    res.status(404).send({ error: `User with id ${id} not found` });
   } else {
-    res.send(database[id]);
+    res.send(database.users[id]);
   }
 });
-
-
 
 app.post('/register', async (req, res) => {
   const { email, password, admin } = req.body;
@@ -56,10 +56,9 @@ app.post('/register', async (req, res) => {
   } else {
     try {
       const hashedPassword = await bcrypt.hash(password, saltRounds);
-      const id = nextId++;
-      database[id] = { id, email, password: hashedPassword, admin: !!admin };
+      const id = nextUserId++;
+      database.users[id] = { id, email, password: hashedPassword, admin: !!admin };
       res.send({ message: `Registered email ${email} with id ${id}` });
-      console.log(database);
     } catch (error) {
       res.status(500).send({ error: "Internal server error" });
     }
@@ -71,7 +70,7 @@ app.post('/login', async (req, res) => {
   if (!validateEmail(email)) {
     res.status(400).send({ error: "Invalid email" });
   } else {
-    const user = Object.values(database).find((user) => user.email === email);
+    const user = Object.values(database.users).find((user) => user.email === email);
     if (!user) {
       res.status(404).send({ error: "Email not found" });
     } else {
@@ -90,196 +89,150 @@ app.post('/login', async (req, res) => {
   }
 });
 
-
 const verifyToken = (req, res, next) => {
   const token = req.headers['x-access-token'];
   if (!token) {
-    return res.status(403).send({ error: 'No token provided' });
+  return res.status(403).send({ error: 'No token provided' });
   }
   jwt.verify(token, jwtSecret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ error: 'Unauthorized!' });
-    }
-    req.userId = decoded.id;
-    next();
+  if (err) {
+  return res.status(401).send({ error: 'Unauthorized!' });
+  }
+  req.userId = decoded.id;
+  next();
   });
-};
-
-
-
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user')
-})
-app.delete('/user/:id', verifyToken, (req, res) => {
+  };
+  
+  app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user');
+  });
+  
+  app.delete('/user/:id', verifyToken, (req, res) => {
   const userId = req.userId;
   const targetId = req.params.id;
-
-  if (!database[userId]) {
+  
+  if (!database.users[userId]) {
     res.status(404).send({ error: `User with id ${userId} not found` });
-  } else if (!database[userId].admin) {
-    res.status(403).send({ error: 'You do not have permission to delete other accounts' });
-  } else if (!database[targetId]) {
+
+  } else if (!database.users[userId].admin) {
+  res.status(403).send({ error: 'You do not have permission to delete other accounts' });
+  } else if (!database.users[targetId]) {
     res.status(404).send({ error: `User with id ${targetId} not found` });
+
   } else {
-    const { email } = database[targetId];
-    delete database[targetId];
-    res.send({ message: `Deleted user ${email} with id ${targetId}` });
-    console.log(database);
-  }
-});
+  const { email } = database.users[targetId];
+  delete database.users[targetId];
+  res.send({ message: `Deleted user ${email} with id ${targetId}` });
 
-app.get('/users', verifyToken, (req, res) => {
+  }
+  });
+  
+  app.get('/users', verifyToken, (req, res) => {
   const { active, field1, value1, field2, value2 } = req.query;
-  let filteredUsers = Object.values(database);
-
+  let filteredUsers = Object.values(database.users);
+  
   if (active !== undefined) {
-    const isActive = active.toLowerCase() === 'true';
-    filteredUsers = filteredUsers.filter((user) => user.active === isActive);
+  const isActive = active.toLowerCase() === 'true';
+  filteredUsers = filteredUsers.filter((user) => user.active === isActive);
   }
-
+  
   if (field1 && value1) {
-    filteredUsers = filteredUsers.filter((user) => user[field1] === value1);
+  filteredUsers = filteredUsers.filter((user) => user[field1] === value1);
   }
-
+  
   if (field2 && value2) {
-    filteredUsers = filteredUsers.filter((user) => user[field2] === value2);
+  filteredUsers = filteredUsers.filter((user) => user[field2] === value2);
   }
-
-  res.send(filteredUsers.map(({ id, email, admin, active }) => ({ id, email, admin, active })));
-});
-
-app.get('/api/user/profile', verifyToken, (req, res) => {
+  
+  res.send(filteredUsers.map(({ id, email, admin }) => ({ id, email, admin })));
+  });
+  
+  app.get('/api/user/profile', verifyToken, (req, res) => {
   const userId = req.userId;
-
-  if (!database[userId]) {
+  
+  if (!database.users[userId]) {
     res.status(404).send({ error: `User with id ${userId} not found` });
   } else {
-    // Hardcoded response since the database is not ready yet
-    const userProfile = {
-      id: userId,
-      email: database[userId].email,
-      meals: [
-        {
-          id: 1,
-          title: "Pasta Bolognese",
-          date: "2023-05-01",
-          time: "18:00",
-          description: "Delicious homemade pasta with Bolognese sauce",
-          imageUrl: "https://example.com/pasta-bolognese.jpg",
-          vegetarian: false,
-          vegan: false
-        },
-        {
-          id: 2,
-          title: "Vegan Burger",
-          date: "2023-05-02",
-          time: "19:00",
-          description: "Tasty vegan burger with fresh vegetables and homemade sauce",
-          imageUrl: "https://example.com/vegan-burger.jpg",
-          vegetarian: true,
-          vegan: true
-        }
-      ]
-    };
-
-    res.send({ message: `Retrieved profile for user ${userId}`, data: userProfile });
-  }
+  const userProfile = {
+  id: userId,
+  email: database.users[userId].email,
+  meals: Object.values(database.meals).filter((meal) => meal.userId === userId),
+  };
+  res.send({ message: `Retrieved profile for user ${userId}`, data: userProfile });
+}
 });
 
 app.get('/api/user/:userId', verifyToken, (req, res) => {
   const requestedUserId = req.params.userId;
   const loggedInUserId = req.userId;
-
-  if (!database[requestedUserId]) {
+  
+  if (!database.users[requestedUserId]) {
     res.status(404).send({ error: `User with id ${requestedUserId} not found` });
   } else {
-    // Hardcoded response since the database is not ready yet
-    const userDetails = {
-      id: requestedUserId,
-      email: database[requestedUserId].email,
-      meals: [
-        {
-          id: 1,
-          title: "Pasta Bolognese",
-          date: "2023-05-01",
-          time: "18:00",
-          description: "Delicious homemade pasta with Bolognese sauce",
-          imageUrl: "https://example.com/pasta-bolognese.jpg",
-          vegetarian: false,
-          vegan: false
-        },
-        {
-          id: 2,
-          title: "Vegan Burger",
-          date: "2023-05-02",
-          time: "19:00",
-          description: "Tasty vegan burger with fresh vegetables and homemade sauce",
-          imageUrl: "https://example.com/vegan-burger.jpg",
-          vegetarian: true,
-          vegan: true
-        }
-      ]
-    };
-
-    if (requestedUserId === loggedInUserId) {
-      userDetails.password = database[requestedUserId].password;
-    }
-
-    res.send({ message: `Retrieved details for user ${requestedUserId}`, data: userDetails });
+  const userDetails = {
+  id: requestedUserId,
+  email: database.users[requestedUserId].email,
+  meals: Object.values(database.meals).filter((meal) => meal.userId === requestedUserId),
+  };
+  if (requestedUserId === loggedInUserId) {
+  userDetails.password = database.users[requestedUserId].password;
   }
+  res.send({ message: `Retrieved details for user ${requestedUserId}`, data: userDetails });
+
+}
 });
 
 app.put('/api/user/:userId', verifyToken, (req, res) => {
-  const requestedUserId = req.params.userId;
-  const loggedInUserId = req.userId;
+const requestedUserId = req.params.userId;
+const loggedInUserId = req.userId;
 
-  if (!database[requestedUserId]) {
-    res.status(404).send({ error: `User with id ${requestedUserId} not found` });
-  } else if (requestedUserId !== loggedInUserId) {
-    res.status(403).send({ error: 'You are not allowed to update this user data' });
+
+res.send({ message: `Retrieved details for user ${requestedUserId}`, data: userDetails });
+
+});
+
+app.put('/api/user/:userId', verifyToken, (req, res) => {
+const requestedUserId = req.params.userId;
+const loggedInUserId = req.userId;
+if (!database.users[requestedUserId]) {
+  res.status(404).send({ error: `User with id ${requestedUserId} not found` });
+} else if (requestedUserId !== loggedInUserId) {
+  res.status(403).send({ error: 'You are not allowed to update this user data' });
   } else {
-    // Validate the input data
-    const { email, phoneNumber } = req.body;
-
-    if (!email) {
-      res.status(400).send({ error: 'Email address is required' });
-    } else if (!isValidPhoneNumber(phoneNumber)) {
-      res.status(400).send({ error: 'Phone number is not valid' });
-    } else {
-      // Hardcoded update since the database is not ready yet
-      database[requestedUserId].email = email;
-      database[requestedUserId].phoneNumber = phoneNumber;
-
-      res.send({ message: `Updated details for user ${requestedUserId}`, data: database[requestedUserId] });
-    }
+  // Validate the input data
+  const { email, phoneNumber } = req.body;
+  if (!email) {
+    res.status(400).send({ error: 'Email address is required' });
+  } else if (!isValidPhoneNumber(phoneNumber)) {
+    res.status(400).send({ error: 'Phone number is not valid' });
+  } else {
+    database.users[requestedUserId].email = email;
+    database.users[requestedUserId].phoneNumber = phoneNumber;
+  
+    res.send({ message: `Updated details for user ${requestedUserId}`, data: database.users[requestedUserId] });
   }
+}
 });
 
 function isValidPhoneNumber(phoneNumber) {
-  // Add your phone number validation logic here
-  // This is a simple example that checks if the phone number is 10 digits long
-  return phoneNumber && phoneNumber.length === 10;
+// Add your phone number validation logic here
+// This is a simple example that checks if the phone number is 10 digits long
+return phoneNumber && phoneNumber.length === 10;
 }
 
 app.delete('/api/user/:userId', verifyToken, (req, res) => {
-  const requestedUserId = req.params.userId;
-  const loggedInUserId = req.userId;
-
-  if (!database[requestedUserId]) {
-    res.status(404).send({ error: `User with id ${requestedUserId} not found` });
-  } else if (requestedUserId !== loggedInUserId) {
-    res.status(403).send({ error: 'You are not allowed to delete this user data' });
-  } else {
-    // Hardcoded deletion since the database is not ready yet
-    delete database[requestedUserId];
-
-    res.send({ message: `User with ID ${requestedUserId} has been deleted` });
-  }
+const requestedUserId = req.params.userId;
+const loggedInUserId = req.userId;
+if (!database.users[requestedUserId]) {
+  res.status(404).send({ error: `User with id ${requestedUserId} not found` });
+} else if (requestedUserId !== loggedInUserId) {
+res.status(403).send({ error: 'You are not allowed to delete this user data' });
+} else {
+delete database.users[requestedUserId];
+res.send({ message: `User with ID ${requestedUserId} has been deleted` });
+}
 });
-
-
-
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+  console.log(`Example app listening at http://localhost:${port}`);
+});  
