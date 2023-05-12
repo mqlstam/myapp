77ce6abb4ebc1204a9097936e498093be81d8906
Miquel Stam <mql.stam@gmail.com>
2023-05-13 00:06:59 +0200

    fix info test

--- test/integration/info.test.js
+++ test/integration/info.test.js
@@ -21,8 +21,8 @@ describe('UC-102 Informatie opvragen', function () {
         res.body.should.has.property('data');
         let { data, message } = res.body;
         data.should.be.an('object');
-        data.should.has.property('studentName').to.be.equal('Davide');
-        data.should.has.property('studentNumber').to.be.equal(1234567);
+        data.should.has.property('studentName').to.be.equal('Miquel');
+        data.should.has.property('studentNumber').to.be.equal(2159021);
         done();
       });
   });
